0b0e3ee49cc079ac5404f24b6cd65ca15d95d19f
lashundra <lashundrasdavis@att.net>
2023-03-16 22:15:48 -0500

    added ngFor and orbit-list code

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Satellite } from '../satellite';
 
 @Component({
-  selector: 'app-orbit-list',
+  selector: 'orbit-list',
   templateUrl: './orbit-list.component.html',
   styleUrls: ['./orbit-list.component.css']
 })
@@ -10,7 +10,9 @@ import { Satellite } from '../satellite';
 export class OrbitListComponent implements OnInit {
 
   @Input() satellites: Satellite[];
+  
   constructor() { }
+
 
   ngOnInit() {
   }
